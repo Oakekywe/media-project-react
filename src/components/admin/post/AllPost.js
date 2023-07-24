cb5c7529005560d6abfe0e0b19d25fc55d01d5ca
oakekywe <oakekywe.zotefamily@gmail.com>
2023-07-24 14:18:32 +0630

    [add] all post with paginate

--- src/components/admin/post/AllPost.js
+++ src/components/admin/post/AllPost.js
@@ -5,13 +5,16 @@ import UiPost from "./UiPost";
 
 const AllPost = () => {
   const [posts, setPosts] = useState([]);
+  const [page, setPage] = useState(1);
+  const [totalPage, setTotalPage] = useState(1);
 
   const loadPosts = async () => {
-    let res = await fetch(API_URL + "/posts");
+    let res = await fetch(API_URL + `/posts/paginate/${page}`);
     let data = await res.json();
 
     if (data.con) {
-      setPosts(data.data);
+      setPosts(data.data.data);
+      setTotalPage(data.data.last_page);
     } else {
       console.log("errors");
     }
@@ -19,7 +22,7 @@ const AllPost = () => {
 
   useEffect(() => {
     loadPosts();
-  }, []);
+  }, [page]);
 
   //delete method
   const apiDeletePost = async (id) => {
@@ -37,16 +40,60 @@ const AllPost = () => {
     }
   };
 
+  const increase = () => {
+    if (page < totalPage) {
+      setPage((prev) => prev + 1);
+    }
+  };
+  const decrease = () => {
+    if (page >= 2) {
+      setPage((prev) => prev - 1);
+    }
+  };
+
   return (
     <div>
-      <Link to="/admin/posts/add" className="btn btn-sm btn-primary my-3">
-        Add
-      </Link>
+      <div className="d-flex justify-content-between my-2">
+        <div>
+          <Link to="/admin/posts/add" className="btn btn-sm btn-primary">
+            Add
+          </Link>
+        </div>
+        <nav aria-label="Page navigation example">
+          <ul className="pagination pagination-sm">
+            <button
+              onClick={() => decrease()}
+              className="page-item page-link me-1"
+              disabled={page < totalPage}
+            >
+              Prev
+            </button>
+            <button onClick={() => increase()} className="page-item page-link" disabled={page >= totalPage}>
+              Next
+            </button>
+          </ul>
+        </nav>
+      </div>
       <div className="row">
         {posts.length > 0 &&
           posts.map((post) => (
             <UiPost key={post.id} post={post} apiDeletePost={apiDeletePost} />
           ))}
+      </div>
+      <div className="d-flex justify-content-center mt-3">
+        <nav aria-label="Page navigation example">
+          <ul className="pagination pagination-sm">
+            <button
+              onClick={() => decrease()}
+              className="page-item page-link me-1"
+            >
+              Prev
+            </button>
+            <button onClick={() => increase()} className="page-item page-link">
+              Next
+            </button>
+          </ul>
+        </nav>
       </div>
     </div>
   );
